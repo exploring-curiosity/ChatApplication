f8d279cec332ffc8a721ded8e3d9dc50ed292153
exploring-curiosity <curiositywake@gmail.com>
2021-05-16 11:57:13 +0530

    started chat application

---
+++ chat-application/src/components/Chat/Chat.js
@@ -0,0 +1,30 @@
+import React,{useEffect,useState} from 'react';
+import './chat.css';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+let socket;
+const Chat = () => {
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+    useEffect(()=>{
+        const {name,room} =queryString.parse(window.location.search);
+        var connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000,                  
+            "transports" : ["websocket"]
+        };
+        socket = io(ENDPOINT,connectionOptions);
+        console.log(socket);
+        setName(name);
+        setRoom(room);
+    },[])
+    return ( 
+        <div>
+            <h1>Chat</h1>
+        </div> 
+    );
+}
+ 
+export default Chat;
---
+++ chat-application/src/components/Join/Join.js
@@ -0,0 +1,21 @@
+import React,{ useState } from 'react';
+import {Link} from 'react-router-dom';
+import './join.css';
+const Join = () => {
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+    return ( 
+        <div className='JoinOuter'>
+            <div className = 'JoinInner'>
+                <h1 className='heading'>Join</h1>
+                <div><input type='text' placeholder='Name' id='name' name='name' autoComplete='off' value={name} onChange={(e)=>{setName(e.target.value)}} /></div>
+                <div><input type='text' placeholder='Room' id='room' name='room' autoComplete='off' value={room} onChange={(e)=>{setRoom(e.target.value)}} /></div>
+                <Link onClick={(e)=>(!name || !room)?e.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className='joinbutton' type='submit'>Sign In</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+ 
+export default Join;
